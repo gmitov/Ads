e48e470bea09878f50305ad4d919094da0379075
gmitov <georgi.iliev.mitov@gmail.com>
2015-01-01 18:51:14 +0200

    Added Registration and login form UI

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,13 @@
 var addsApp = angular.module('addsAppModule', ['ngRoute'])
 .config(function($routeProvider){
     $routeProvider.when('/login', {
-        templateUrl:'',
-        controller: 'MainController'
-    })
+        templateUrl:'templates/login.html'
+    });
+    $routeProvider.when('/register', {
+        templateUrl:'templates/register.html'
+    });
+    $routeProvider.when('/ads', {
+        templateUrl:'templates/all-ads.html'
+    });
+    $routeProvider.otherwise({redirectTo: '/ads'})
 });
